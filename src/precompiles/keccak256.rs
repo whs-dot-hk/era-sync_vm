cfc0824ee96acd3a5dd79d948b170667760dfe30
whs <hswongac@gmail.com>
2024-01-13 21:41:16 +0800

    Test

--- src/precompiles/keccak256.rs
+++ src/precompiles/keccak256.rs
@@ -106,13 +106,13 @@ struct Keccak256VarCore {
     state: Keccak256InnerState,
 }
 
-fn transmute_state(reference_state: sha3::Keccak256) -> Keccak256InnerState {
-    // we use a trick that size of both structures is the same, and even though we do not know a stable field layout,
-    // we can replicate it
-    let our_wrapper: CoreWrapper = unsafe { std::mem::transmute(reference_state) };
-
-    our_wrapper.core.state
-}
+//fn transmute_state(reference_state: sha3::Keccak256) -> Keccak256InnerState {
+//    // we use a trick that size of both structures is the same, and even though we do not know a stable field layout,
+//    // we can replicate it
+//    let our_wrapper: CoreWrapper = unsafe { std::mem::transmute(reference_state) };
+//
+//    our_wrapper.core.state
+//}
 
 pub struct KeccakSelfVerifier {
     internal_state: sha3::Keccak256,
